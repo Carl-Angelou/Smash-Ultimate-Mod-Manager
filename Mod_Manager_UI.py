6f1d9491c24b9ad4258b53845065103e963b0af2
Carl-Angelou <carlangelou06@gmail.com>
2024-01-26 21:01:41 -0500

    Frame, Right Menu, & Start ListFile Function

--- Mod_Manager_UI.py
+++ Mod_Manager_UI.py
@@ -2,8 +2,9 @@ from tkinter import *
 from tkinter import filedialog
 from screeninfo import get_monitors
 import pymsgbox
+import os
 
-# ---------------- Monitor resolution & display default message box ----------------------
+# ---------------- Monitor resolution & display default message box ---------------------- TODO: MAKE IT APPLY TO FILE DIALOG (make file dialog at center of screen)
 primary_mon_width = 0
 primary_mon_height = 0
 primary_mon_x = 0
@@ -20,25 +21,65 @@ print('primary_mon_height: ' + str(primary_mon_height))
 print('primary_mon_x: ' + str(primary_mon_x))
 
 # Changes default message box location to half height and width of primary display
-pymsgbox.rootWindowPosition = '+' + str(int(primary_mon_width/3)) + '+' + str(int(primary_mon_height/3))
+pymsgbox.rootWindowPosition = '+' + str(int(primary_mon_width/10)) + '+' + str(int(primary_mon_height/3))
 
-# ---------------- MAIN Window Functionality ----------------------
+# -------------------- MAIN Window Properties ----------------------
 root = Tk() # Create Root (MAIN) Window
+
+# GLOBAL VARIABLES
+global filename
+filename = 'N/A'
 
 # Naming and Sizing
 root.title("Smash Ultimate Mod Manager")
-root.minsize(int(primary_mon_width/3), int(primary_mon_height/2)) # TODO: MAKE CODE MORE CONCISE
-root.maxsize(int(primary_mon_width/3), int(primary_mon_height/2))
+root.resizable(0,0)
 root.geometry(str(int(primary_mon_width/3)) + 'x' + str(int(primary_mon_height/2)) + '+' + str(int(primary_mon_width/3)) + '+' + str(int(primary_mon_height/6)))
 
+# -------------------- Functions for Clickable Objects ----------------------
 def BrowseFiles(): # https://www.geeksforgeeks.org/file-explorer-in-python-using-tkinter/
-    filename = filedialog.askdirectory(initialdir='/', title='Select a Folder')
+    filename1 = filedialog.askdirectory(initialdir='/', title='Select a Folder') # https://pythonassets.com/posts/browse-file-or-folder-in-tk-tkinter/
+    filename = filename1
+    print('\nBrowse File - Filename: ' + filename)
 
-# Button Layout
-button_file_browse = Button(root, text='Browse Files', command=BrowseFiles)
+def ListFiles(): # https://www.javatpoint.com/file-explorer-using-tkinter-in-python
+    file_list = os.listdir(os.path.abspath(filename))
 
-# Grid Layout
-button_file_browse.grid(column=1, row=1)
+# -------------------- Menu Layout ----------------------
+# Frame Layout
+Tk.grid(root)
+root.columnconfigure(0, weight=5)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(0, weight=1)
+
+menu_left = Frame(root, bg='red') # Left Menu Frame
+menu_left.grid(row=0, column=0, sticky='nsew')
+
+menu_right = Frame(root, bg='gold') # Right Menu Frame
+menu_right.grid(row=0, column=1, sticky='nsew')
+
+# Right Menu Layout
+menu_right.rowconfigure(0, weight=1) # https://www.youtube.com/watch?v=i577cFu8eBI (skip to end of video)
+menu_right.rowconfigure(1, weight=1) # Row 1: Used for BROWSE FILES Button
+menu_right.rowconfigure(2, weight=10)
+menu_right.rowconfigure(3, weight=1) # Row 2: Used for SKINS Button
+menu_right.rowconfigure(4, weight=1) # Row 3: Used for STAGES Button
+menu_right.rowconfigure(5, weight=5)
+menu_right.columnconfigure(0, weight=1)
+
+label_file_browse = Label(menu_right, text='Please select your Smash\nUltimate Mods folder...', font=('verdana', '10'))
+label_file_browse.grid(row=0, column=0)
+
+button_file_browse = Button(menu_right, text='Browse Files', font='verdana', command=BrowseFiles)
+button_file_browse.grid(row=1, column=0)
+
+button_skins = Button(menu_right, text='Skins', font='verdana', width=13, height=5, command=BrowseFiles)
+button_skins.grid(row=2, column=0)
+
+button_stages = Button(menu_right, text='Stages', font='verdana', width=13, height=5, command=BrowseFiles)
+button_stages.grid(row=3, column=0)
+
+# Left Menu Layout
+
 
 root.mainloop()
 
